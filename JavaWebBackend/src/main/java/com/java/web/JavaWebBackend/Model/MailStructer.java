5b83d22730a6aae8eb1bf825b882272edc9b32a3
berkaykrks <ata.karakus@std.medipol.edu.tr>
2024-05-24 12:24:26 +0300

    güncellendi

---
+++ JavaWebBackend/src/main/java/com/java/web/JavaWebBackend/Model/MailStructer.java
@@ -0,0 +1,33 @@
+package com.java.web.JavaWebBackend.Model;
+
+public class MailStructer {
+
+	private String subject;
+	private String mail;
+	private String phone;
+	private String write;
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getWrite() {
+		return write;
+	}
+	public void setWrite(String write) {
+		this.write = write;
+	}
+}
